b32818ccf6b24a65769311968212ecf0c6af724d
mattcirkovic <matthewcirkovic@gmail.com>
2020-03-01 17:21:20 -0700

    made changes to super_block

--- src/disk.h
+++ src/disk.h
@@ -3,8 +3,26 @@ const block_size = 4096;
 
 struct super_block
 {
-	int block_num;
-	int inode_num;
-	int free_blk;
-	int free_inode;
+	int magic_number;
+	int blocks; //total number of blocks
+	int inode_blocks; //number of blocks set aside for inode
+	int inodes; // total number of inodes within block
+}
+
+struct inode{ //store file metadata
+	int type;
+	int uid;
+	float size;
+	float time;
+	float ctime;
+	float mtime;
+	float blocks;
+}
+
+struct data_map{
+	//could be either bitmap or linked list
+}
+
+struct inode_bitmap{
+
 }
