1d3a183545c35c44c092bb14c0602821c8091d1a
mattcirkovic <matthewcirkovic@gmail.com>
2020-03-09 14:41:33 -0600

    Removed header file for now

--- src/FSfS.cpp
+++ src/FSfS.cpp
@@ -1,11 +1,39 @@
-#include "disk.h"
+// #include "disk.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
+const int MAGIC_NUMBER = 123456;
+const int BLOCK_SIZE = 4096;
+const int MAX_NAME_SIZE = 50;
+
+struct Super_block{
+	int magic_number;
+	int blocks; //number of blocks
+};
+
+struct partition{
+	int begin, end;
+};
+
+class Disk{
+	public:
+		Super_block super_block;
+		string name;
+		Disk(string, int);
+};
+
+Disk::Disk(string disk_name, int block_num){
+	name = disk_name;
+	super_block.magic_number = MAGIC_NUMBER;
+	super_block.blocks = block_num;
+}
+
+
 int main(){
-  Disk disk("newdisk", 10);
-  cout << disk.name << " has " << disk.super_block.blocks << " blocks.";
-  system("pause");
-  return 0;
+	string name = "newDisk";
+	Disk disk(name, 10);
+	cout << disk.name << " has " << disk.super_block.blocks << " blocks.";
+	system("pause");
+	return 0;
 }
--- src/disk.h
+++ src/disk.h
@@ -1,3 +1,6 @@
+#include <string>
+using namespace std;
+
 const int MAGIC_NUMBER = 123456;
 const int BLOCK_SIZE = 4096;
 const int MAX_NAME_SIZE = 50;
@@ -14,11 +17,11 @@ struct partition{
 class Disk{
 	public:
 		Super_block super_block;
-		char* name;
-		Disk(char[], int);
+		string name;
+		Disk(string, int);
 };
 
-Disk::Disk(char* disk_name, int block_num){
+Disk::Disk(string disk_name, int block_num){
 	name = disk_name;
 	super_block.magic_number = MAGIC_NUMBER;
 	super_block.blocks = block_num;
